9e0886471841e30392a1aa892a063f5e4e8e6988
longhuawhu <longhua1949hm@163.com>
2015-07-23 09:43:18 +0800

    Initial Commit

---
+++ BMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMapDemo
+//
+//  Created by lh on 15/7/23.
+//  Copyright (c) 2015年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMapDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMapDemo
+//
+//  Created by lh on 15/7/23.
+//  Copyright (c) 2015年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
